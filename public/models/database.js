655390e1a11d5941d38a9d8ede2ad6c74ebf14e4
ceditoph <CR.patterson2000@gmail.com>
2015-08-22 17:31:21 -0700

    changed vote to array

--- public/models/database.js
+++ public/models/database.js
@@ -3,6 +3,6 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/po
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE IF NOT EXISTS topics(id serial NOT NULL,text character varying(160) NOT NULL,vote integer NOT NULL,CONSTRAINT topics_pkey PRIMARY KEY (id));');
+var query = client.query('CREATE TABLE IF NOT EXISTS topics(id serial NOT NULL,text character varying(160) NOT NULL,vote integer[] NOT NULL,CONSTRAINT topics_pkey PRIMARY KEY (id));');
 
 module.exports = client;
